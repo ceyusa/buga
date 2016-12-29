47340941207e97bbb042a4a7bcd09496b1c71151
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2016-12-29 19:28:26 +0100

    scripts: lower to 20% the scale

--- scripts/process-images.sh
+++ scripts/process-images.sh
@@ -4,10 +4,10 @@ process() {
     image=$1
     echo "processing ${image}"
     mv ${image} "o${image}"
-    convert "o${image}" -scale 30% -strip -quality 86 -interlace Plane ${image}
+    convert "o${image}" -scale 20% -strip -quality 86 -interlace Plane ${image}
 }
 
-for i in *.jpg
+for i in $( find . -maxdepth 1 -iname "*.JPG" -printf '%f\n' )
 do
     process $i
 done
